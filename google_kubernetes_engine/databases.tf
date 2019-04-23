5a4f9faf54aeccd267ebc787bb0ecd68c5894c6a
Phil Dier <phil@dier.us>
2019-04-23 14:36:27 -0500

    add TODO note about issues when destroying the sql instance

--- google_kubernetes_engine/databases.tf
+++ google_kubernetes_engine/databases.tf
@@ -29,6 +29,13 @@ resource "google_sql_user" "codecov" {
   password = "${random_string.postgres-password.result}"
 }
 
+# TODO destroying this resource fails because GCP refuses to destroy user above
+# while it still owns db resources.  For now, if you need to destroy the entire
+# stack, either remove the database instance manually, or remove the tf state for
+# the above user to allow the db instance to be destroyed.
+# ex: 
+# terraform state rm google_sql_user.codecov
+# terraform destroy
 resource "google_sql_database" "codecov" {
   name = "codecov"
   instance = "${google_sql_database_instance.codecov.name}"
